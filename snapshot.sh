fd91d23cb6017688029eb7b3bd61702569dfccf8
kuilin <git-commit@kuilin.net>
2025-08-17 00:56:31 -0400

    move efi snapshots to main for space

--- snapshot.sh
+++ snapshot.sh
@@ -9,9 +9,10 @@ sudo -v
 cd /mnt/btrfsroot/boot
 [[ ! -e @$1 ]]
 sudo btrfs sub snap @ @$1
-cd @$1
-sudo dd if=/dev/disk/by-label/EFI of=efi.bin status=progress
 
 cd /mnt/btrfsroot/nixos
 [[ ! -e @$1 ]]
 sudo btrfs sub snap @ @$1
+
+cd @$1
+sudo dd if=/dev/disk/by-label/EFI of=efi.bin status=progress
